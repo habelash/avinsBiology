a99e43d16a2fe9381b3bbdd9cf2a3125db5780df
habelash <habelash@gmail.com>
2020-06-23 18:44:34 +0530

    iframes problem

--- lectures/models.py
+++ lectures/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class LectureVideos(models.Model):
+    topic = models.CharField(max_length=50)
+    disc = models.TextField()
+    link = models.URLField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Topics'
